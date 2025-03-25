af0f7197f29e10822800b9111a1cd81279ea1c12
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-03-25 23:20:34 +0530

    user files Changed commit

--- AngularUdemyTutorials/src/app/user/user.component.ts
+++ AngularUdemyTutorials/src/app/user/user.component.ts
@@ -24,6 +24,7 @@ export class UserComponent {
   @Input({required:true}) id !: string;
   @Input({required:true}) avatar !: string;
   @Input({required:true}) name !: string;
+  @Input({ required: true }) selected !: boolean;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
   //  avatar = input.required<string>();

0d1558cc268c0b60b8df191234468361118776d4
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-03-27 00:05:13 +0530

    new Task Changes

--- AngularUdemyTutorials/src/app/tasks/newtask/newtask.component.ts
+++ AngularUdemyTutorials/src/app/tasks/newtask/newtask.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-newtask',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './newtask.component.html',
   styleUrl: './newtask.component.css'
 })
 export class NewtaskComponent {
 
+  @Output() cancel = new EventEmitter<void>();
+  // @Output() add = new EventEmitter<{title:string;summary:string;date:string;}>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  onCancel(){
+    this.cancel.emit();
+  }
+
+  onSubmit(){
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate
+    });
+  }
 }

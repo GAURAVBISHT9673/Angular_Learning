3d6017aaf4180d79f5afde026bb31ae40846d8c9
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-06-05 08:14:17 +0530

    committed 05 June 2025

--- LawReading/src/app/app.component.ts
+++ LawReading/src/app/app.component.ts
@@ -12,4 +12,32 @@ import { NavbarComponent } from "./navbar/navbar.component";
 })
 export class AppComponent {
   title = 'LawReading';
+
+  blog=[
+    {
+      img: 'blog-p-1.webp',
+      date: '20 January, 2020',
+      heading: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+      subheading: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.'
+    },
+    {
+      img: 'blog-p-2.jpg',
+      date: '20 January, 2020',
+      heading: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+      subheading: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.'
+    },
+    {
+      img: 'blog-p-3.jpg',
+      date: '20 January, 2020',
+      heading: 'Lorem ipsum, dolor sit amet consectetur adipisicing',
+      subheading: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis libero quas ipsum laudantium nihil! Quaerat.'
+    }
+  ];
+
+
+  changetheColor(){
+    
+  }
+
+
 }

1a34650749c61e7be7dead10d1ca8b63ecd08129
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-06-14 08:13:11 +0530

    14 June 2025

--- KDCollectionUI/src/app/main/main.component.ts
+++ KDCollectionUI/src/app/main/main.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MainComponent {
 

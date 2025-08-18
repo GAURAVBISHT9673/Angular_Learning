b3bdcef302ef6d5ea826ef760d04c179fd3fdd48
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-08-18 21:12:33 +0530

    18 aug 2025

--- KDCollectionUI/src/app/app.component.ts
+++ KDCollectionUI/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MainComponent } from "./main/main.component";
 import { ProductsComponent } from "./products/products.component";
+import { NewsletterComponent } from "./newsletter/newsletter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, ProductsComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, ProductsComponent, NewsletterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

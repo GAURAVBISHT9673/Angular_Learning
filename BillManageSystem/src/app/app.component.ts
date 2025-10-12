0ef88c9eccf84324da23c1f539f05ba1fe105f07
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-10-12 22:16:14 +0530

    12-oct-2025

--- BillManageSystem/src/app/app.component.ts
+++ BillManageSystem/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftSideNavBarComponent } from "./left-side-nav-bar/left-side-nav-bar.component";
+
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [LeftSideNavBarComponent, RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'BillManageSystem';
+  // title = 'BillManageSystem';
+
+
 }
---
+++ BillManageSystem/src/app/left-side-nav-bar/left-side-nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-left-side-nav-bar',
+  imports: [RouterLink],
+  templateUrl: './left-side-nav-bar.component.html',
+  styleUrl: './left-side-nav-bar.component.scss'
+})
+export class LeftSideNavBarComponent {
+
+}

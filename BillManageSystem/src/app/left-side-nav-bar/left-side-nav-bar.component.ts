0774991ebb05945372ddbffea7468202409651e7
GAURAVBISHT9673 <jaharbisht@gmail.com>
2025-10-14 22:47:29 +0530

    14-oct-2025

--- BillManageSystem/src/app/left-side-nav-bar/left-side-nav-bar.component.ts
+++ BillManageSystem/src/app/left-side-nav-bar/left-side-nav-bar.component.ts
@@ -1,12 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-left-side-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './left-side-nav-bar.component.html',
   styleUrl: './left-side-nav-bar.component.scss'
 })
 export class LeftSideNavBarComponent {
+
+  iconName: string = 'bx-chevron-down';
+  showMenu: string = '';
+  toggleIcon() {
+    console.log("Icon clicked");
+    this.iconName = this.iconName === 'bx-chevron-down' ? 'bx-chevron-up' : 'bx-chevron-down';
+    this.showMenu = this.showMenu === '' ? 'showMenu' : '';
+  }
 
 }
